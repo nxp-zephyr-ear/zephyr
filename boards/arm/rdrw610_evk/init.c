5bf070bb84427d13c568eed5f150c63a9071a198
Declan Snyder <declan.snyder@nxp.com>
2022-10-17 10:07:05 -0500

    [nxp noup] boards: arm: Add RDRW610 Evaluation Kit
    Add RDRW610_EVK board.
    
    Supports blinky, button, and hello world samples.
    
    Signed-off-by: Declan Snyder <declan.snyder@nxp.com>

---
+++ boards/arm/rdrw610_evk/init.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2022 NXP
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/devicetree.h>
+#include <zephyr/init.h>
+
+#include "fsl_io_mux.h"
+
+static int rdrw610_evk_init(void)
+{
+#if (DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(flexcomm3), nxp_lpc_usart, okay)) && CONFIG_SERIAL
+	IO_MUX_SetPinMux(IO_MUX_FC3_USART_DATA);
+#endif
+#if CONFIG_GPIO
+	IO_MUX_SetPinMux(IO_MUX_GPIO2);
+	IO_MUX_SetPinMux(IO_MUX_GPIO25);
+	IO_MUX_SetPinMux(IO_MUX_GPIO52);
+#endif
+	return 0;
+}
+
+SYS_INIT(rdrw610_evk_init, PRE_KERNEL_1, CONFIG_BOARD_INIT_PRIORITY);
