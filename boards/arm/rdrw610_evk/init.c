4e73271fda6e169ef97f945bfa8e770b69700634
Declan Snyder <declan.snyder@nxp.com>
2022-11-15 10:06:19 -0600

    [nxp noup] soc: Move RW610 clocking to soc
    To be consistent with other NXP soc, move clocking for this
    soc back to soc level
    
    Signed-off-by: Declan Snyder <declan.snyder@nxp.com>

--- boards/arm/rdrw610_evk/init.c
+++ boards/arm/rdrw610_evk/init.c
@@ -11,29 +11,6 @@
 
 static int rdrw610_evk_init(void)
 {
-	const clock_frg_clk_config_t debug_clock_conf = {3, kCLOCK_FrgPllDiv, 255, 0};
-
-#if (DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(flexcomm3), nxp_lpc_usart, okay)) && CONFIG_SERIAL
-	CLOCK_SetFRGClock(&debug_clock_conf);
-	CLOCK_AttachClk(kFRG_to_FLEXCOMM3);
-#endif
-
-#if (DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(flexcomm2), nxp_lpc_i2c, okay)) && CONFIG_I2C
-	CLOCK_AttachClk(kSFRO_to_FLEXCOMM2);
-#endif
-
-
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(usb_otg), okay) && CONFIG_USB_DC_NXP_EHCI
-	/* Enable system xtal from Analog */
-	SYSCTL2->ANA_GRP_CTRL |= (1UL << SYSCTL2_ANA_GRP_CTRL_PU_SHIFT);
-	/* reset USB */
-	RESET_PeripheralReset(kUSB_RST_SHIFT_RSTn);
-	/* enable usb clock */
-	CLOCK_EnableClock(kCLOCK_Usb);
-	/* enable usb phy clock */
-	CLOCK_EnableUsbhsPhyClock();
-#endif
-
 	return 0;
 }
 
