b1682e57375afca05cfe64728e3064360b4f84e5
Declan Snyder <declan.snyder@nxp.com>
2023-06-30 10:51:43 -0500

    [nxp noup] include: sensor.h: Add comparator channel
    Add comparator as a sensor channel. This channel type represents the
    binary output of a comparator circuit.
    
    Signed-off-by: Declan Snyder <declan.snyder@nxp.com>

---
+++ include/zephyr/drivers/sensor/comparator.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2023 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DRIVERS_SENSOR_COMPARATOR_H_
+#define ZEPHYR_INCLUDE_DRIVERS_SENSOR_COMPARATOR_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define SENSOR_ATTR_SLOPE_TH_FALLING 0
+#define SENSOR_ATTR_SLOPE_TH_RISING 1
+#define SENSOR_ATTR_THRESH_LOW 0
+#define SENSOR_ATTR_THRESH_HIGH 1
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_INCLUDE_DRIVERS_SENSOR_COMPARATOR_H_ */
