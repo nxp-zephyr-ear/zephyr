346aa158e21a6bb48f4229553480f6b143b9d54d
Yves Vandervennet <yves.vandervennet@nxp.com>
2023-08-07 10:01:55 -0500

    [nxp noup] rw610: enablement of linkserver
    added:
     - board definitions
     - common linkserver definitions
    
    Signed-off-by: Yves Vandervennet <yves.vandervennet@nxp.com>

--- boards/arm/rdrw610_evk/board.cmake
+++ boards/arm/rdrw610_evk/board.cmake
@@ -3,4 +3,7 @@
 
 board_runner_args(jlink "--device=RW610" "--reset-after-load")
 
+board_runner_args(linkserver  "--device=RW610:RDRW610")
+
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/linkserver.board.cmake)
