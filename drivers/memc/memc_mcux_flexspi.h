e096d1ec389a300727ebbd20bae78b1731f5c438
Daniel DeGrasse <daniel.degrasse@nxp.com>
2023-10-23 09:39:08 -0500

    Revert "[nxp fromlist] drivers: flash: flash_flexspi_nor: support SFDP probe"
    This reverts commit d418a38d1ae75b86415f52bef44f62da7417a795.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- drivers/memc/memc_mcux_flexspi.h
+++ drivers/memc/memc_mcux_flexspi.h
@@ -10,8 +10,6 @@
 
 /* Size of a command in the LUT table */
 #define MEMC_FLEXSPI_CMD_SIZE 4U
-/* Number of commands in an instruction sequence */
-#define MEMC_FLEXSPI_CMD_PER_SEQ 4U
 
 /**
  * @brief Wait for the FlexSPI bus to be idle
