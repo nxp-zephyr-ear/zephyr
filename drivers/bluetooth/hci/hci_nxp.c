a2ccade22abc1a84cf4c205df74ffb74be4020ae
Daniel DeGrasse <daniel.degrasse@nxp.com>
2023-10-23 09:39:03 -0500

    Revert "[nxp noup] drivers: hci_nxp: add support of controller auto sleep mode"
    This reverts commit 7c088ad136b8de641a095450d80bc69c99821a08.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- drivers/bluetooth/hci/hci_nxp.c
+++ drivers/bluetooth/hci/hci_nxp.c
@@ -14,7 +14,6 @@
 #include <zephyr/sys/byteorder.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/flash.h>
-#include <zephyr/bluetooth/hci_types.h>
 #include <soc.h>
 
 #include "fwk_platform_ble.h"
@@ -23,169 +22,44 @@
 /* -------------------------------------------------------------------------- */
 /*                                  Definitions                               */
 /* -------------------------------------------------------------------------- */
-
 #define DT_DRV_COMPAT nxp_hci_ble
 
 #define LOG_LEVEL CONFIG_BT_HCI_DRIVER_LOG_LEVEL
 LOG_MODULE_REGISTER(bt_driver);
 
-#define HCI_IRQ_N        DT_INST_IRQ_BY_IDX(0, 0, irq)
-#define HCI_IRQ_P        DT_INST_IRQ_BY_IDX(0, 0, priority)
-#define HCI_WAKEUP_IRQ_N DT_INST_IRQ_BY_IDX(0, 1, irq)
-#define HCI_WAKEUP_IRQ_P DT_INST_IRQ_BY_IDX(0, 1, priority)
+#define HCI_IRQ_N DT_INST_IRQN(0)
+#define HCI_IRQ_P DT_INST_IRQ(0, priority)
 
-#define HCI_CMD 0x01
-#define HCI_ACL 0x02
-#define HCI_EVT 0x04
-
-/* Vendor specific commands */
-#define HCI_CMD_PACKET_HEADER_LENGTH                    3U
-#define HCI_CMD_VENDOR_OCG                              0x3FU
-#define HCI_CMD_STORE_BT_CAL_DATA_OCF                   0x61U
-#define HCI_CMD_STORE_BT_CAL_DATA_PARAM_LENGTH          32U
-#define HCI_CMD_STORE_BT_CAL_DATA_ANNEX100_OCF          0xFFU
-#define HCI_CMD_STORE_BT_CAL_DATA_PARAM_ANNEX100_LENGTH 16U
-#define HCI_CMD_SET_BT_SLEEP_MODE_OCF                   0x23U
-#define HCI_CMD_SET_BT_SLEEP_MODE_PARAM_LENGTH          3U
-#define HCI_CMD_BT_HIU_HS_ENABLE_OCF                    0x5A
-#define HCI_CMD_BT_HOST_SLEEP_CONFIG_OCF                0x59U
-#define HCI_CMD_BT_HOST_SLEEP_CONFIG_PARAM_LENGTH       2U
+#define HCI_CMD         0x01
+#define HCI_ACL	        0x02
+#define HCI_EVT	        0x04
 
 #if defined(HCI_NXP_SET_BDADDR)
 
 #if !defined(BD_ADDR_OUI)
-#define BD_ADDR_OUI 0x37U, 0x60U, 0x00U
+#define BD_ADDR_OUI                             0x37U, 0x60U, 0x00U
 #endif
 
-#define PLATFORM_BLE_BD_ADDR_OUI_PART_SIZE 3U
-#define BLE_DEVICE_ADDR_SIZE               (6U)
-#define HCI_SET_MAC_ADDR_CMD_LENGTH        (8U)
-#define HCI_CMD_PACKET_HEADER_LENGTH       (3U)
-#define HCI_VENDOR_SPECIFIC_DBG_CMD        0x03FU
-#define HCI_SET_MAC_ADDR_CMD               0x0022U
-#define BT_USER_BD                         254
-#define HCI_COMMAND(opCodeGroup, opCodeCommand)                                                    \
-	(((uint16_t)(opCodeGroup) & (uint16_t)0x3FU) << (uint16_t)SHIFT10) |                       \
-		(uint16_t)((opCodeCommand)&0x3FFU)
+#define PLATFORM_BLE_BD_ADDR_OUI_PART_SIZE      3U
+#define	BLE_DEVICE_ADDR_SIZE                    (6U)
+#define HCI_SET_MAC_ADDR_CMD_LENGTH             (8U)
+#define HCI_CMD_PACKET_HEADER_LENGTH            (3U)
+#define HCI_VENDOR_SPECIFIC_DBG_CMD             0x03FU
+#define HCI_SET_MAC_ADDR_CMD                    0x0022U
+#define BT_USER_BD 254
+#define HCI_COMMAND(opCodeGroup, opCodeCommand)\
+	(((uint16_t)(opCodeGroup) & (uint16_t)0x3FU)<<(uint16_t)SHIFT10) \
+	|(uint16_t)((opCodeCommand) & 0x3FFU)
 
 #endif /* HCI_NXP_SET_BDADDR */
 
-extern int32_t ble_hci_handler(void);
-extern int32_t ble_wakeup_done_handler(void);
+/* IMU Interrupt priority */
+#define RF_IMU0_IRQPriority                     2
 
+extern int32_t ble_wakeup_handler(void);
 /* -------------------------------------------------------------------------- */
 /*                             Private functions                              */
 /* -------------------------------------------------------------------------- */
-
-#if CONFIG_HCI_NXP_ENABLE_AUTO_SLEEP
-static int nxp_bt_send_vs_command(uint16_t opcode, const uint8_t *params, uint8_t params_len)
-{
-	struct net_buf *buf;
-
-	/* Allocate buffer for the hci command */
-	buf = bt_hci_cmd_create(opcode, params_len);
-	if (buf == NULL) {
-		LOG_ERR("Unable to allocate command buffer");
-		return -ENOMEM;
-	}
-
-	/* Add data part of packet */
-	net_buf_add_mem(buf, params, params_len);
-
-	/* Send the command */
-	return bt_hci_cmd_send_sync(opcode, buf, NULL);
-}
-
-static int nxp_bt_enable_controller_autosleep(void)
-{
-	uint16_t opcode = BT_OP(BT_OGF_VS, HCI_CMD_SET_BT_SLEEP_MODE_OCF);
-	const uint8_t params[HCI_CMD_SET_BT_SLEEP_MODE_PARAM_LENGTH] = {
-		0x02U, /* Auto sleep enable */
-		0x00U, /* Idle timeout LSB */
-		0x00U  /* Idle timeout MSB */
-	};
-
-	/* Send the command */
-	return nxp_bt_send_vs_command(opcode, params, HCI_CMD_SET_BT_SLEEP_MODE_PARAM_LENGTH);
-}
-
-static int nxp_bt_set_host_sleep_config(void)
-{
-	uint16_t opcode = BT_OP(BT_OGF_VS, HCI_CMD_BT_HOST_SLEEP_CONFIG_OCF);
-	const uint8_t params[HCI_CMD_BT_HOST_SLEEP_CONFIG_PARAM_LENGTH] = {
-		0xFFU, /* BT_HIU_WAKEUP_INBAND */
-		0xFFU, /* BT_HIU_WAKE_GAP_WAIT_FOR_IRQ */
-	};
-
-	/* Send the command */
-	return nxp_bt_send_vs_command(opcode, params, HCI_CMD_BT_HOST_SLEEP_CONFIG_PARAM_LENGTH);
-}
-#endif /* CONFIG_HCI_NXP_ENABLE_AUTO_SLEEP */
-
-#ifdef CONFIG_HCI_NXP_SET_BDADDR
-static void bt_nxp_set_address(void)
-{
-	uint8_t bleDeviceAddress[BLE_DEVICE_ADDR_SIZE] = {0};
-	uint8_t uid[16] = {0};
-	uint8_t uuidLen;
-	uint8_t addrOUI[PLATFORM_BLE_BD_ADDR_OUI_PART_SIZE] = {BD_ADDR_OUI};
-	/* Set BD address by HCI message */
-	uint8_t hciPacket[HCI_SET_MAC_ADDR_CMD_LENGTH + HCI_CMD_PACKET_HEADER_LENGTH];
-	uint16_t opcode = HCI_COMMAND(HCI_VENDOR_SPECIFIC_DBG_CMD, HCI_SET_MAC_ADDR_CMD);
-	uint8_t hciBuffer[HCI_SET_MAC_ADDR_CMD_LENGTH + HCI_CMD_PACKET_HEADER_LENGTH + 1U];
-	uint32_t packetSize = HCI_CMD_PACKET_HEADER_LENGTH + HCI_SET_MAC_ADDR_CMD_LENGTH;
-
-	memcpy((void *)hciPacket, (const void *)&opcode, 2U);
-	/* Set HCI parameter length */
-	hciPacket[2] = (uint8_t)HCI_SET_MAC_ADDR_CMD_LENGTH;
-	/* Set command parameter ID */
-	hciPacket[3] = (uint8_t)BT_USER_BD;
-	/* Set command parameter length */
-	hciPacket[4] = (uint8_t)6U;
-	/* Use UID to create a unique MAC address */
-	PLATFORM_GetMCUUid(uid, &uuidLen);
-	/* Set 3 LSB of MAC address from UUID */
-	if (uuidLen >= PLATFORM_BLE_BD_ADDR_OUI_PART_SIZE) {
-		memcpy((void *)bleDeviceAddress,
-			   (void *)(uid + uuidLen - PLATFORM_BLE_BD_ADDR_OUI_PART_SIZE),
-			   PLATFORM_BLE_BD_ADDR_OUI_PART_SIZE);
-	}
-	/* Set 3 MSB of MAC address from OUI */
-	memcpy((void *)(bleDeviceAddress + PLATFORM_BLE_BD_ADDR_OUI_PART_SIZE), (void *)addrOUI,
-		   PLATFORM_BLE_BD_ADDR_OUI_PART_SIZE);
-	memcpy((void *)&hciPacket[HCI_CMD_PACKET_HEADER_LENGTH + 2U],
-		   (const void *)bleDeviceAddress, BLE_DEVICE_ADDR_SIZE);
-	hciBuffer[0] = HCI_CMD;
-	memcpy(hciBuffer + 1U, (const void *)hciPacket, packetSize);
-
-	PLATFORM_SendHciMessage(hciBuffer, packetSize + 1);
-}
-#endif /* HCI_NXP_SET_BDADDR */
-
-static bool is_hci_event_discardable(const uint8_t *evt_data)
-{
-	bool ret = false;
-	uint8_t evt_type = evt_data[0];
-
-	switch (evt_type) {
-	case BT_HCI_EVT_LE_META_EVENT: {
-		uint8_t subevt_type = evt_data[sizeof(struct bt_hci_evt_hdr)];
-
-		switch (subevt_type) {
-		case BT_HCI_EVT_LE_ADVERTISING_REPORT:
-			ret = true;
-			break;
-		default:
-			break;
-		}
-	} break;
-
-	default:
-		break;
-	}
-
-	return ret;
-}
 
 static struct net_buf *bt_evt_recv(uint8_t *data, size_t len)
 {
@@ -193,6 +67,7 @@ static struct net_buf *bt_evt_recv(uint8_t *data, size_t len)
 	uint8_t payload_len;
 	uint8_t evt_hdr;
 	bool discardable_evt;
+	uint8_t subevt_type;
 	size_t space_in_buffer;
 
 	payload_len = data[1];
@@ -209,8 +84,13 @@ static struct net_buf *bt_evt_recv(uint8_t *data, size_t len)
 		return NULL;
 	}
 
-	discardable_evt = is_hci_event_discardable(data);
-
+	/* Check if the event can be discarded */
+	if (evt_hdr == BT_HCI_EVT_LE_META_EVENT) {
+		subevt_type = data[BT_HCI_EVT_HDR_SIZE];
+		if (subevt_type == BT_HCI_EVT_LE_ADVERTISING_REPORT) {
+			discardable_evt = true;
+		}
+	}
 	/* Allocate a buffer for the HCI Event */
 	buf = bt_buf_get_evt(evt_hdr, discardable_evt, K_NO_WAIT);
 
@@ -225,12 +105,8 @@ static struct net_buf *bt_evt_recv(uint8_t *data, size_t len)
 		/* Copy the data to the buffer */
 		net_buf_add_mem(buf, data, len);
 	} else {
-		if (discardable_evt) {
-			LOG_DBG("Discardable buffer pool full, ignoring event");
-		} else {
-			LOG_ERR("No available event buffers!");
-		}
-		return buf;
+		LOG_ERR("Event buffer is empty!");
+		return NULL;
 	}
 
 	return buf;
@@ -296,6 +172,46 @@ static void hci_rx_cb(uint8_t packetType, uint8_t *data, uint16_t len)
 	}
 }
 
+#if defined(HCI_NXP_SET_BDADDR)
+static void bt_nxp_set_address(void)
+{
+	uint8_t bleDeviceAddress[BLE_DEVICE_ADDR_SIZE] = {0};
+	uint8_t uid[16] = {0};
+	uint8_t uuidLen;
+	uint8_t addrOUI[PLATFORM_BLE_BD_ADDR_OUI_PART_SIZE] = {BD_ADDR_OUI};
+	/* Set BD address by HCI message */
+	uint8_t hciPacket[HCI_SET_MAC_ADDR_CMD_LENGTH + HCI_CMD_PACKET_HEADER_LENGTH];
+	uint16_t opcode = HCI_COMMAND(HCI_VENDOR_SPECIFIC_DBG_CMD, HCI_SET_MAC_ADDR_CMD);
+	uint8_t hciBuffer[HCI_SET_MAC_ADDR_CMD_LENGTH + HCI_CMD_PACKET_HEADER_LENGTH + 1U];
+	uint32_t packetSize = HCI_CMD_PACKET_HEADER_LENGTH + HCI_SET_MAC_ADDR_CMD_LENGTH;
+
+	memcpy((void *)hciPacket, (const void *)&opcode, 2U);
+	/* Set HCI parameter length */
+	hciPacket[2] = (uint8_t)HCI_SET_MAC_ADDR_CMD_LENGTH;
+	/* Set command parameter ID */
+	hciPacket[3] = (uint8_t)BT_USER_BD;
+	/* Set command parameter length */
+	hciPacket[4] = (uint8_t)6U;
+	/* Use UID to create a unique MAC address */
+	PLATFORM_GetMCUUid(uid, &uuidLen);
+	/* Set 3 LSB of MAC address from UUID */
+	if (uuidLen >= PLATFORM_BLE_BD_ADDR_OUI_PART_SIZE) {
+		memcpy((void *)bleDeviceAddress,
+			(void *)(uid + uuidLen - PLATFORM_BLE_BD_ADDR_OUI_PART_SIZE),
+			PLATFORM_BLE_BD_ADDR_OUI_PART_SIZE);
+	}
+	/* Set 3 MSB of MAC address from OUI */
+	memcpy((void *)(bleDeviceAddress + PLATFORM_BLE_BD_ADDR_OUI_PART_SIZE),
+		(void *)addrOUI, PLATFORM_BLE_BD_ADDR_OUI_PART_SIZE);
+	memcpy((void *)&hciPacket[HCI_CMD_PACKET_HEADER_LENGTH + 2U],
+		(const void *)bleDeviceAddress, BLE_DEVICE_ADDR_SIZE);
+	hciBuffer[0] = HCI_CMD;
+	memcpy(hciBuffer + 1U, (const void *)hciPacket, packetSize);
+
+	PLATFORM_SendHciMessage(hciBuffer, packetSize + 1);
+}
+#endif /* HCI_NXP_SET_BDADDR */
+
 static int bt_nxp_send(struct net_buf *buf)
 {
 	uint8_t packetType;
@@ -324,41 +240,10 @@ static int bt_nxp_send(struct net_buf *buf)
 
 static int bt_nxp_open(void)
 {
-	int ret = 0;
 
-	do {
-		ret = PLATFORM_SetHciRxCallback(hci_rx_cb);
-		if (ret < 0) {
-			LOG_ERR("BLE HCI RX callback registration failed");
-			break;
-		}
+	PLATFORM_SetHciRxCallback(hci_rx_cb);
 
-		ret = PLATFORM_StartHci();
-		if (ret < 0) {
-			LOG_ERR("HCI open failed");
-			break;
-		}
-
-#if CONFIG_HCI_NXP_SET_BDADDR
-		bt_nxp_set_address();
-#endif /* HCI_NXP_SET_BDADDR */
-
-#if CONFIG_HCI_NXP_ENABLE_AUTO_SLEEP
-		ret = nxp_bt_set_host_sleep_config();
-		if (ret < 0) {
-			LOG_ERR("Failed to set host sleep config");
-			break;
-		}
-
-		ret = nxp_bt_enable_controller_autosleep();
-		if (ret < 0) {
-			LOG_ERR("Failed to configure controller autosleep");
-			break;
-		}
-#endif
-	} while (false);
-
-	return ret;
+	return 0;
 }
 
 static int bt_nxp_close(void)
@@ -368,12 +253,13 @@ static int bt_nxp_close(void)
 	return 0;
 }
 
+
 static const struct bt_hci_driver drv = {
-	.name = "BT NXP",
-	.open = bt_nxp_open,
-	.close = bt_nxp_close,
-	.send = bt_nxp_send,
-	.bus = BT_HCI_DRIVER_BUS_IPM,
+	.name		= "BT NXP",
+	.open		= bt_nxp_open,
+	.close		= bt_nxp_close,
+	.send		= bt_nxp_send,
+	.bus		= BT_HCI_DRIVER_BUS_IPM,
 };
 
 static int bt_nxp_init(void)
@@ -381,13 +267,9 @@ static int bt_nxp_init(void)
 	int status;
 	int ret = 0;
 
-	/* HCI Interrupt */
-	IRQ_CONNECT(HCI_IRQ_N, HCI_IRQ_P, ble_hci_handler, 0, 0);
+	/*HCI Interrupt*/
+	IRQ_CONNECT(HCI_IRQ_N, HCI_IRQ_P, ble_wakeup_handler, 0, 0);
 	irq_enable(HCI_IRQ_N);
-
-	/* Wake up done interrupt */
-	IRQ_CONNECT(HCI_WAKEUP_IRQ_N, HCI_WAKEUP_IRQ_P, ble_wakeup_done_handler, 0, 0);
-	irq_enable(HCI_WAKEUP_IRQ_N);
 
 	do {
 		status = PLATFORM_InitBle();
@@ -396,7 +278,9 @@ static int bt_nxp_init(void)
 			ret = status;
 			break;
 		}
-
+#if defined(HCI_NXP_SET_BDADDR)
+		bt_nxp_set_address();
+#endif /* HCI_NXP_SET_BDADDR */
 		status = bt_hci_driver_register(&drv);
 		if (status < 0) {
 			LOG_ERR("HCI driver registration failed");
