4ac081f21faecd66fb26068626e5528948d64f53
Daniel DeGrasse <daniel.degrasse@nxp.com>
2023-10-23 09:39:07 -0500

    Revert "[nxp noup] soc: rw6xx: add clock settings for all flexcomm instances"
    This reverts commit e5197fde5490ee26df40cc899443b247d33807cd.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- soc/arm/nxp_imx/rw6xx/soc.c
+++ soc/arm/nxp_imx/rw6xx/soc.c
@@ -176,21 +176,20 @@ static ALWAYS_INLINE void clock_init(void)
 	CLOCK_AttachClk(kFRG_to_FLEXCOMM0);
 #endif
 #if (DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(flexcomm1), nxp_lpc_usart, okay)) && CONFIG_SERIAL
-	CLOCK_SetFRGClock(&(const clock_frg_clk_config_t){1, kCLOCK_FrgPllDiv, 255, 0});
 	CLOCK_AttachClk(kFRG_to_FLEXCOMM1);
 #endif
 #if (DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(flexcomm2), nxp_lpc_usart, okay)) && CONFIG_SERIAL
-	CLOCK_SetFRGClock(&(const clock_frg_clk_config_t){2, kCLOCK_FrgPllDiv, 255, 0});
 	CLOCK_AttachClk(kFRG_to_FLEXCOMM2);
 #endif
 #if (DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(flexcomm3), nxp_lpc_usart, okay)) && CONFIG_SERIAL
-	CLOCK_SetFRGClock(&(const clock_frg_clk_config_t){3, kCLOCK_FrgPllDiv, 255, 0});
 	CLOCK_AttachClk(kFRG_to_FLEXCOMM3);
 #endif
 #if (DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(flexcomm14), nxp_lpc_usart, okay)) && CONFIG_SERIAL
-	CLOCK_SetFRGClock(&(const clock_frg_clk_config_t){14, kCLOCK_FrgPllDiv, 255, 0});
 	CLOCK_AttachClk(kFRG_to_FLEXCOMM14);
 #endif
+	/* Set up USART clock to debug configuration by default */
+	const clock_frg_clk_config_t debug_clock_conf = {3, kCLOCK_FrgPllDiv, 255, 0};
+	CLOCK_SetFRGClock(&debug_clock_conf);
 
 /* Any flexcomm can be I2C */
 #if (DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(flexcomm0), nxp_lpc_i2c, okay)) && CONFIG_I2C
