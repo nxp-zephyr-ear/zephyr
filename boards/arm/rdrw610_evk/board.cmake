9fb952566156322bcb05fc0bd230575740e3a6a4
Daniel DeGrasse <daniel.degrasse@nxp.com>
2023-10-23 09:39:21 -0500

    Revert "[nxp noup] boards: arm: Add RDRW610 Evaluation Kit"
    This reverts commit 60aa560b71b726a827972bbf27bef8d28ed77134.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- boards/arm/rdrw610_evk/board.cmake
+++
@@ -1,6 +0,0 @@
-# Copyright 2022 NXP
-# SPDX-License-Identifier: Apache-2.0
-
-board_runner_args(jlink "--device=RW610" "--reset-after-load")
-
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
