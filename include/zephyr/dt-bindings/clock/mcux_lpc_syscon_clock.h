cd7ec231f6689bf56baf97fb7c6ad6706b8772a0
Declan Snyder <declan.snyder@nxp.com>
2023-11-13 10:19:26 -0600

    [nxp noup] include: dt-bindings: lpc_syscon: Fix RW defines
    RW defines are getting conflicts with things picked from upstream,
    move them to a completely different range to avoid this happening.
    
    Signed-off-by: Declan Snyder <declan.snyder@nxp.com>

--- include/zephyr/dt-bindings/clock/mcux_lpc_syscon_clock.h
+++ include/zephyr/dt-bindings/clock/mcux_lpc_syscon_clock.h
@@ -57,11 +57,10 @@
 #define MCUX_FLEXSPI_CLK		36
 #define MCUX_FLEXSPI2_CLK		37
 
-/* RW IP */
-#define MCUX_GAU_CLK			38
-
 #define MCUX_MRT_CLK			40
 
-#define MCUX_LCDIC_CLK			39
+/* RW IP */
+#define MCUX_GAU_CLK			800
+#define MCUX_LCDIC_CLK			801
 
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_MCUX_LPC_SYSCON_H_ */
