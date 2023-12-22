b8bb80a67ea9d0ae768f325fe293011883ae7f10
Fengming Ye <frank.ye@nxp.com>
2023-12-22 15:09:57 +0900

    [nxp noup] net: zperf: fix download IPv4 address cannot bind to IPv6
    If download bind address is IPv4, only create IPv4 socket and vice versa.
    Add UDP download multicast address support.
    Join mcast group before bind and leave after download stop.
    Need to add interface name in zperf udp download command.
    
    Signed-off-by: Fengming Ye <frank.ye@nxp.com>

--- subsys/net/lib/zperf/zperf_tcp_receiver.c
+++ subsys/net/lib/zperf/zperf_tcp_receiver.c
@@ -147,12 +147,16 @@ static void tcp_server_session(void)
 	static struct zsock_pollfd fds[SOCK_ID_MAX];
 	static struct sockaddr sock_addr[SOCK_ID_MAX];
 	int ret;
+	int family;
 
 	for (int i = 0; i < ARRAY_SIZE(fds); i++) {
 		fds[i].fd = -1;
 	}
 
-	if (IS_ENABLED(CONFIG_NET_IPV4)) {
+	family = tcp_server_addr.sa_family;
+
+	if (IS_ENABLED(CONFIG_NET_IPV4) &&
+		(family == AF_INET || family == AF_UNSPEC)) {
 		struct sockaddr_in *in4_addr = zperf_get_sin();
 		const struct in_addr *addr = NULL;
 
@@ -207,7 +211,8 @@ use_any_ipv4:
 		}
 	}
 
-	if (IS_ENABLED(CONFIG_NET_IPV6)) {
+	if (IS_ENABLED(CONFIG_NET_IPV6) &&
+		(family == AF_INET6 || family == AF_UNSPEC)) {
 		struct sockaddr_in6 *in6_addr = zperf_get_sin6();
 		const struct in6_addr *addr = NULL;
 
