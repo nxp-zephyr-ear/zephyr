f5a83d8ab1d679062e43da9c1c8d1f622413be5c
Daniel DeGrasse <daniel.degrasse@nxp.com>
2024-02-26 09:08:56 -0600

    Revert "[nxp fromlist] net: sockets: Update msg_controllen in recvmsg properly"
    This reverts commit b3c767377e7e72f4a2ddec9d02b77c21754db049.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- subsys/net/lib/sockets/sockets.c
+++ subsys/net/lib/sockets/sockets.c
@@ -1429,20 +1429,12 @@ static inline ssize_t zsock_recv_dgram(struct net_context *ctx,
 		}
 	}
 
-	if (msg != NULL) {
-		if (msg->msg_control != NULL) {
-			if (msg->msg_controllen > 0) {
-				if (IS_ENABLED(CONFIG_NET_CONTEXT_RECV_PKTINFO) &&
-				    net_context_is_recv_pktinfo_set(ctx)) {
-					if (add_pktinfo(ctx, pkt, msg) < 0) {
-						msg->msg_flags |= ZSOCK_MSG_CTRUNC;
-					}
-				} else {
-					msg->msg_controllen = 0U;
-				}
+	if (msg != NULL && msg->msg_control != NULL && msg->msg_controllen > 0) {
+		if (IS_ENABLED(CONFIG_NET_CONTEXT_RECV_PKTINFO) &&
+		    net_context_is_recv_pktinfo_set(ctx)) {
+			if (add_pktinfo(ctx, pkt, msg) < 0) {
+				msg->msg_flags |= ZSOCK_MSG_CTRUNC;
 			}
-		} else {
-			msg->msg_controllen = 0U;
 		}
 	}
 
@@ -1893,10 +1885,6 @@ ssize_t z_vrfy_zsock_recvmsg(int sock, struct msghdr *msg, int flags)
 			K_OOPS(k_usermode_to_copy(msg->msg_control,
 						  msg_copy.msg_control,
 						  msg_copy.msg_controllen));
-
-			msg->msg_controllen = msg_copy.msg_controllen;
-		} else {
-			msg->msg_controllen = 0U;
 		}
 
 		k_usermode_to_copy(&msg->msg_iovlen,
