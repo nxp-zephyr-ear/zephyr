a0f04a85e82f96d6610ab5691164c4487a6271bf
Daniel DeGrasse <daniel.degrasse@nxp.com>
2024-02-26 09:09:38 -0600

    Revert "[nxp noup] boards: arm: Move flexcomm3 clocking to board init"
    This reverts commit dc82a8d57d4d849190982541fbb98619aea41d1a.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- boards/arm/rdrw610_evk/init.c
+++ boards/arm/rdrw610_evk/init.c
@@ -6,25 +6,18 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/init.h>
 
-#include <fsl_clock.h>
 #include "fsl_io_mux.h"
 
 static int rdrw610_evk_init(void)
 {
-	const clock_frg_clk_config_t debug_clock_conf = {3, kCLOCK_FrgPllDiv, 255, 0};
-
 #if (DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(flexcomm3), nxp_lpc_usart, okay)) && CONFIG_SERIAL
-	CLOCK_SetFRGClock(&debug_clock_conf);
-	CLOCK_AttachClk(kFRG_to_FLEXCOMM3);
 	IO_MUX_SetPinMux(IO_MUX_FC3_USART_DATA);
 #endif
-
 #if CONFIG_GPIO
 	IO_MUX_SetPinMux(IO_MUX_GPIO2);
 	IO_MUX_SetPinMux(IO_MUX_GPIO25);
 	IO_MUX_SetPinMux(IO_MUX_GPIO52);
 #endif
-
 	return 0;
 }
 
