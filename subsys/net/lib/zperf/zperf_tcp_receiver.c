0a620bf5b83743ba40e3a09a108ea5a44b43e213
Fengming Ye <frank.ye@nxp.com>
2024-03-18 17:44:47 +0900

    Revert "[nxp noup] net: zperf: fix download IPv4 address cannot bind to IPv6"
    This reverts commit aa3a894e5573ea0a4576b9a06ce5835d2562f0e0.

--- subsys/net/lib/zperf/zperf_tcp_receiver.c
+++ subsys/net/lib/zperf/zperf_tcp_receiver.c
@@ -280,16 +280,12 @@ static int zperf_tcp_receiver_init(void)
 {
 	int ret = 0;
 	int if_namelen;
-	int family;
 
 	for (int i = 0; i < ARRAY_SIZE(fds); i++) {
 		fds[i].fd = -1;
 	}
 
-	family = tcp_server_addr.sa_family;
-
-	if (IS_ENABLED(CONFIG_NET_IPV4) &&
-		(family == AF_INET || family == AF_UNSPEC)) {
+	if (IS_ENABLED(CONFIG_NET_IPV4)) {
 		struct sockaddr_in *in4_addr = zperf_get_sin();
 		const struct in_addr *addr = NULL;
 
@@ -351,8 +347,7 @@ use_any_ipv4:
 		}
 	}
 
-	if (IS_ENABLED(CONFIG_NET_IPV6) &&
-		(family == AF_INET6 || family == AF_UNSPEC)) {
+	if (IS_ENABLED(CONFIG_NET_IPV6)) {
 		struct sockaddr_in6 *in6_addr = zperf_get_sin6();
 		const struct in6_addr *addr = NULL;
 
